9332e0325145c7dde4e01362baca4df386b8143f
Hesho-Official <87497482+Hesho-Official@users.noreply.github.com>
2021-08-13 10:49:26 +0530

    Add files via upload

---
+++ req.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("echo '\n \033[91m REQUIREMENT AVAILABLE' ")
+os.system("echo '\033[91mINSTALLING...' ")
+os.system("pkg install cowsay -y")
+os.system("pkg install ruby -y")
+os.system("gem install locat")
+os.system("pkg install neofetch -y")
+os.system("echo '\033[91m PKG INSTALL SUCCESSFULLY...' ")
+os.system("echo '\n\033[94m DONE...!!!' ")
